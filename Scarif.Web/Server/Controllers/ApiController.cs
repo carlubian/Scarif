9bf15c8c2a23ebd48b978b84d68665af2a63426a
Carlos Lubián <carlubian@outlook.es>
2021-02-05 10:19:35 +0100

    🧨V2 recreation

---
+++ Scarif.Web/Server/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scarif.Web.Server.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class ApiController
+    {
+    }
+}
---
+++ Scarif.Web/Server/Hubs/ScarifHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scarif.Web.Server.Hubs
+{
+    public class ScarifHub : Hub
+    {
+        public void Test(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
